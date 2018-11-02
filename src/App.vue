8c963cdffd940ee1ce655918103d2be26c5920c8
ZouXin <405495715@qq.com>
2018-11-02 08:40:12 +0800

    🎉 init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <VueVerifySlider @success="successHandler" />
+    <p>{{ successMsg }}</p>
+  </div>
+</template>
+
+<script>
+import VueVerifySlider from './VueVerifySlider'
+export default {
+  components: {
+    VueVerifySlider
+  },
+  data () {
+    return {
+      successMsg: ''
+    }
+  },
+  methods: {
+    successHandler () {
+      this.successMsg = '验证成功回调'
+    }
+  }
+}
+</script>
